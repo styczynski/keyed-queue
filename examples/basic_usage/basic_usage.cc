c0ad0de2bd8eecec54e26fdad4d89dde7be6b143
styczynski <piotrsty1@gmail.com>
2017-12-11 00:02:59 +0100

    Implement somewhre good solution :tada:

--- examples/basic_usage/basic_usage.cc
+++ examples/basic_usage/basic_usage.cc
@@ -12,16 +12,36 @@
 
 int main()
 {
+  std::cout << "A\n";
   keyed_queue<int, int> q1;
+  
+  std::cout << "B\n";
   q1.push(1, 1);
+  
+  std::cout << "C\n";
   std::cout << q1.to_string() << "\n";
   
+  std::cout << "D\n";
   keyed_queue<int, int> q2 = q1;
+  
+  std::cout << "E\n";
   std::cout << q2.to_string() << "\n";
+  
+  std::cout << "F\n";
   q1.push(2, 2);
+  
+  std::cout << "G\n";
   std::cout << q1.to_string() << "\n";
+  
+  std::cout << "H\n";
   q2.push(3, 3);
   std::cout << q2.to_string() << "\n";
+    
+  std::cout << "I\n";
+  q2.move_to_back(1);
+  std::cout << q2.to_string() << "\n";
+  
+  std::cout << "J\n";
     
   return 0;
 }
--- src/keyed_queue.h
+++ src/keyed_queue.h
@@ -15,145 +15,292 @@
 #include <memory>
 #include <iostream>
 #include <sstream>
+#include <assert.h>
 
 class lookup_error : std::exception {
+private:
+    std::string what_message;
 public:
+    lookup_error(std::string additionalDescription) {
+        std::ostringstream out;
+        out << "Keyed queue lookup error. ";
+        out << additionalDescription;
+        what_message = out.str();
+    }
 
-
+    const char* what() {
+        return what_message.c_str();
+    }
 };
 
 template <class K, class V>
 class keyed_queue {
+private:
+    using kv_pair = std::pair<K, V>;
+    using kv_list = std::list<kv_pair>;
+    using kv_list_i = typename kv_list::iterator;
+    using kvi_list = std::list<kv_list_i>;
+    using kvi_list_i = typename kvi_list::iterator;
+    using kvi_map = std::map<K, kvi_list>;
+    using kvi_map_i = typename kvi_map::iterator;
+
+    
 public:
 
     class k_iterator {
+    private:
+        kvi_map_i map_iter;
+        bool assigned;
+        
     public:
-        k_iterator() {
-            //TODO: Implement
-            throw "Not implemented";
+    
+        k_iterator(const kvi_map_i i) {
+            map_iter = i;
+            assigned = true;
+        }
+    
+        k_iterator() noexcept {
+            assigned = false;
         }
         
         k_iterator(k_iterator const& i) {
-            //TODO: Implement
-            throw "Not implemented";
+            assigned = i.assigned;
+            map_iter = i.map_iter;
         }
         
-        void operator++() {
-            //TODO: Implement
-            throw "Not implemented";
+        void operator++() noexcept {
+            if(assigned) ++map_iter;
         }
         
-        bool operator==(k_iterator k) {
-            //TODO: Implement
-            throw "Not implemented";
+        bool operator==(k_iterator k) noexcept {
+            if(!assigned || !k.assigned) return false;
+            return map_iter == k.map_iter;
         }
         
-        bool operator!=(k_iterator k) {
-            //TODO: Implement
-            throw "Not implemented";
+        bool operator!=(k_iterator k) noexcept {
+            if(!assigned || !k.assigned) return true;
+            return map_iter != k.map_iter;
         }
         
         K operator*() {
-            //TODO: Implement
-            throw "Not implemented";
+            if(!assigned) throw "k_iterator: Dereferencing unassigned iterator.";
+            return map_iter->first;
         }
     };
     
 private:
     
     class queue_data {
-    private: 
-        std::map<K, V> keys;
-        std::list<V> fifo;
+    private:
+        kvi_map keys;
+        kv_list fifo;
 
     public:
+      
+        queue_data(const queue_data& q) {
+            std::cout << "Copy queue\n";
+            for(auto i=q.fifo.begin(); i!=q.fifo.end(); ++i) {
+                fifo.push_back(*i);
+                auto el_i = fifo.end();
+                --el_i;
+                const auto l = keys.insert({ i->first, {} });
+                (l.first)->second.push_back(el_i);
+            }
+        }
         
         queue_data(): keys(), fifo() {
             
         }
         
-        void push(K const &k, V const &v) {
-            keys[k] = v;
+        queue_data(queue_data&& q): keys(std::move(q.keys)),
+                                    fifo(std::move(q.fifo)) {
+        
         }
         
-        void pop(K const &k, V const &v) {
+        void push(K const &k, V const &v) {
+            
+            //TODO: This do not provide ANY exception guarantee!
+            // Idk check this shit out plz
+            
+            kv_list fifo_delta;
+            
+            try {
+                fifo_delta.push_back({k, v}); // < This may fail!
+            } catch(...) {
+                throw;
+            }
+            
+            fifo.splice(fifo.end(), fifo_delta); // < Safe
+            
+            const auto l = keys.insert({ k, {} }); // < This may too? Maybe?
+            auto delta_i = fifo.end();
+            --delta_i;
+            (l.first)->second.push_back(delta_i); // < Safe
             
         }
         
         void pop(K const &k) {
-            
+            const auto iter = keys.find(k);
+            if(iter == keys.end()) {
+                throw lookup_error("pop(K): Key not present in the queue.");
+            } else {
+                // TODO: Validate
+                const auto e = (iter->second).front();
+                fifo.erase(e);
+                (iter->second).pop_front();
+                if((iter->second).size() <= 0) {
+                    keys.erase(iter);
+                }
+            }
         }
         
         void pop() {
+            if(fifo.empty()) {
+                throw lookup_error("pop(): Queue is empty.");
+            }
             
+            // TODO: Validate
+            const auto iter = fifo.begin();
+            const auto key = iter->first;
+            
+            const auto i = keys.find(key);
+            assert(i != keys.end());
+            
+            auto& list = i->second;
+            list.pop_front();
+            
+            if(list.size() <= 0) {
+                keys.erase(i);
+            }
+            
+            fifo.pop_front();
         }
         
         void move_to_back(K const &k) {
+            const auto i = keys.find(k);
+            if(i == keys.end()) {
+                throw lookup_error("move_to_back(K): There's no such key in the queue.");
+            }
             
+            // TODO: Validate
+            auto& list = i->second;
+            for(auto e=list.begin();e!=list.end();++e) {
+                fifo.push_back(**e);
+                auto new_e = fifo.end();
+                --new_e;
+                fifo.erase(*e);
+                *e = new_e;
+            }
         }
         
         std::pair<K const &, V &> front() {
-            
+            if(fifo.empty()) {
+                throw lookup_error("front(): Queue is empty.");
+            }
+            auto& ref = fifo.front();
+            return { std::get<0>(ref), std::get<1>(ref) };
         }
         
         std::pair<K const &, V &> back() {
-            
+            if(fifo.empty()) {
+                throw lookup_error("back(): Queue is empty.");
+            }
+            auto& ref = fifo.back();
+            return { std::get<0>(ref), std::get<1>(ref) };
         }
         
         std::pair<K const &, V const &> front() const {
-            
+            if(fifo.empty()) {
+                throw lookup_error("front(): Queue is empty.");
+            }
+            auto& ref = fifo.front();
+            return { std::get<0>(ref), std::get<1>(ref) };
         }
         
         std::pair<K const &, V const &> back() const {
-            
+            if(fifo.empty()) {
+                throw lookup_error("back(): Queue is empty.");
+            }
+            auto& ref = fifo.back();
+            return { std::get<0>(ref), std::get<1>(ref) };
         }
         
         std::pair<K const &, V &> first(K const &key) {
-            
+            const auto keyloc = keys.find(key);
+            if(keyloc == keys.end()) {
+                throw lookup_error("first(K): Key not present in the queue.");
+            }
+            return { key, (keyloc->second.front())->second };
         }
         
         std::pair<K const &, V &> last(K const &key) {
-            
+            const auto keyloc = keys.find(key);
+            if(keyloc == keys.end()) {
+                throw lookup_error("last(K): Key not present in the queue.");
+            }
+            return { key, (keyloc->second.back())->second };
         }
         
         std::pair<K const &, V const &> first(K const &key) const {
-            
+            const auto keyloc = keys.find(key);
+            if(keyloc == keys.end()) {
+                throw lookup_error("first(K): Key not present in the queue.");
+            }
+            return { key, (keyloc->second.front())->second };
         }
         
         std::pair<K const &, V const &> last(K const &key) const {
-            
+            const auto keyloc = keys.find(key);
+            if(keyloc == keys.end()) {
+                throw lookup_error("last(K): Key not present in the queue.");
+            }
+            return { key, (keyloc->second.back())->second };
         }
         
-        size_t size() const {
-            return 0;
+        size_t size() const noexcept {
+            return fifo.size();
         }
         
-        bool empty() const {
-            return false;
+        bool empty() const noexcept {
+            return fifo.empty();
         }
         
-        void clear() {
-            
+        void clear() noexcept {
+            fifo.clear();
+            keys.clear();
         }
         
-        size_t count(K const &k) const {
+        size_t count(K const &k) const noexcept {
+            const auto i = keys.find(k);
+            if(i != keys.end()) {
+                return i->second.size();
+            }
             return 0;
         }
         
         k_iterator k_begin() {
-            
+            return k_iterator(keys.begin());
         }
         
         k_iterator k_end() {
-            
+            return k_iterator(keys.end());
         }
         
-        std::string to_string() const {
+        std::string to_string() const noexcept {
             std::ostringstream out;
             
-            out << "{";
-            for(auto e : keys) {
-                out << e.first << ": [" << e.second << "] ";
+            out << "{\n";
+            out << " * keys:\n";
+            for(auto k : keys) {
+              out << "   " << k.first << ": ";
+              for(auto e: k.second) {
+                out << "     " << e->first << "=> " << e->second << "\n";
+              }
+              out << "\n";
+            }
+            out << " * fifo:\n";
+            for(auto e : fifo) {
+                out << "   " << e.first << "=> " << e.second << "\n";
             }
             out << "}";
             
@@ -208,21 +355,21 @@ private:
         }
         
         // Access underlying object
-        queue_data& read() {
+        queue_data& read() noexcept {
             return *shared_data;
         }
         
         // Access underlying object
-        const queue_data& read_const() const {
+        const queue_data& read_const() const noexcept {
             return *shared_data;
         }
         
-        const queue_shared_data& unsharable() const {
+        const queue_shared_data& unsharable() const noexcept {
             *sharable = false;
             return *this;
         }
         
-        bool is_shared() const {
+        bool is_shared() const noexcept {
             return shared_data.use_count() > 1;
         }
     };
@@ -256,12 +403,8 @@ public:
         sd.disjoin().read().pop();
     }
     
-    void pop(K const &k, V const &v) {
-        sd.disjoin().read().pop(k, v);
-    }
-    
     void pop(K const &k) {
-        sd.disjoin().read().pop();
+        sd.disjoin().read().pop(k);
     }
     
     void move_to_back(K const &k) {
@@ -300,19 +443,20 @@ public:
         return sd.unsharable().read_const().last(key);
     }
     
-    size_t size() const {
+    size_t size() const noexcept {
         return sd.read_const().size();
     }
     
-    bool empty() const {
+    bool empty() const noexcept {
         return sd.read_const().empty();
     }
     
+    // NOEXCEPT: Disjoin can potentially throw?
     void clear() {
         sd.disjoin().read().clear();
     }
     
-    size_t count(K const &k) const {
+    size_t count(K const &k) const noexcept {
         return sd.read_const().count(k);
     }
     
@@ -324,7 +468,7 @@ public:
         return sd.read().k_end();
     }
     
-    std::string to_string() const {
+    std::string to_string() const noexcept {
         return ((sd.is_shared())?"<  shared  > ":"<standalone> ") + sd.read_const().to_string();
     }
 };
