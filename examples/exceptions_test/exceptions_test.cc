aedc419c88fc9cab825f41bb4d66c95296c48694
styczynski <piotrsty1@gmail.com>
2017-12-14 20:35:39 +0100

    Update broken tests - all passing

--- examples/exceptions_test/exceptions_test.cc
+++ examples/exceptions_test/exceptions_test.cc
@@ -6,91 +6,91 @@
 
 auto f(testKQ q)
 {
-    return q;
+  return q;
 }
 
 int main () {
 
-    testKQ kq;
+  testKQ kq;
 
-    for (int i = 0; i < 100; ++i) {
-        counter = i;
-        std::unique_ptr<testKQ> kq1, kq2, kq3;
-        std::unique_ptr<testKQ const> kq4;
+  for (int i = 0; i < 100; ++i) {
+    counter = i;
+    std::unique_ptr<testKQ> kq1, kq2, kq3;
+    std::unique_ptr<testKQ const> kq4;
 
-        try {
-            int keys[] = {3, 1, 2};
+    try {
+      int keys[] = {3, 1, 2};
 
-            kq1 = std::make_unique<testKQ>(f({}));
+      kq1 = std::make_unique<testKQ>(f({}));
 
-            for (int i = 0; i < 3; ++i) {
-                kq1->push(keys[i], i);
-            }
+      for (int i = 0; i < 3; ++i) {
+        kq1->push(keys[i], i);
+      }
 
-            auto &ref = kq1->front().second;
+      auto &ref = kq1->front().second;
 
-            kq2 = std::make_unique<testKQ>(*kq1); // Wykonuje się pełna kopia, dlaczego?
-            kq3 = std::make_unique<testKQ>();
-            *kq3 = *kq2;
+      kq2 = std::make_unique<testKQ>(*kq1); // Wykonuje się pełna kopia, dlaczego?
+      kq3 = std::make_unique<testKQ>();
+      *kq3 = *kq2;
 
-            ref.customSetter(10);
-            assert(kq1->front().second.get() == 10);
-            assert(kq2->front().second.get() != 10);
+      ref.customSetter(10);
+      assert(kq1->front().second.get() == 10);
+      assert(kq2->front().second.get() != 10);
 
-            kq2->pop(); // Obiekt kq2 dokonuje kopii i przestaje współdzielić dane z kq3.
-            assert(kq2->size() == 2);
-            assert(kq2->count(3) == 0);
-            assert(kq2->count(2) == 1);
+      kq2->pop(); // Obiekt kq2 dokonuje kopii i przestaje współdzielić dane z kq3.
+      assert(kq2->size() == 2);
+      assert(kq2->count(3) == 0);
+      assert(kq2->count(2) == 1);
 
-            assert(kq3->size() == 3);
-            assert(kq3->count(3) == 1);
+      assert(kq3->size() == 3);
+      assert(kq3->count(3) == 1);
 
-            kq2->push(1, 3);
-            kq2->move_to_back(1);
-            assert(kq2->size() == 3);
-            assert(kq2->front().second.get() == 2
-                   && kq2->first(1).second.get() == 1
-                   && kq2->last(1).second.get() == 3
-                   && kq2->back().second.get() == 3);
+      kq2->push(1, 3);
+      kq2->move_to_back(1);
+      assert(kq2->size() == 3);
+      assert(kq2->front().second.get() == 2
+             && kq2->first(1).second.get() == 1
+             && kq2->last(1).second.get() == 3
+             && kq2->back().second.get() == 3);
 
-            // keyed_queue<int, int> const kq4 = kq2;
-            // assert(kq4.front().second == 2
-            //        && kq4.first(1).second == 1
-            //        && kq4.last(1).second == 3
-            //        && kq4.back().second == 3);
+      // keyed_queue<int, int> const kq4 = kq2;
+      // assert(kq4.front().second == 2
+      //        && kq4.first(1).second == 1
+      //        && kq4.last(1).second == 3
+      //        && kq4.back().second == 3);
 
-            int i = 1;
-            for (auto k_it = kq1->k_begin(), k_end = kq1->k_end();
-                 k_it != k_end;
-                 ++k_it, ++i) {
-                assert(i <= 3 && (*k_it).get() == i);
-            }
+      int i = 1;
+      for (auto k_it = kq1->k_begin(), k_end = kq1->k_end();
+           k_it != k_end;
+           ++k_it, ++i) {
+        assert(i <= 3 && (*k_it).get() == i);
+      }
 
-            // auto kq5 = std::make_unique<keyed_queue<int, int>>();
-            // kq5->push(4, 0);
-            // assert(kq5->front().first == 4 && kq5->front().second == 0);
-            // auto kq6(*kq5);
-            // kq5.reset();
-            // assert(kq6.front().first == 4 && kq6.front().second == 0);
-            //
-            // std::swap(kq1, kq2);
-            // std::vector<keyed_queue<int, int>> vec;
-            // for (int i = 0; i < 100000; i++) {
-            //   kq1.push(i, i);
-            // }
-            // for (int i = 0; i < 1000000; i++) {
-            //   vec.push_back(kq1);  // Wszystkie obiekty w vec współdzielą dane.
-            // }
-        }
-        catch (char const *e) {
-            // std::cout << "blah\n";
-            if (kq1) kq1->compare();
-            if (kq2) kq2->compare();
-            if (kq3) kq3->compare();
-        }
-        std::cout << "Passes " << i << "-th exception's test\n";
+      // auto kq5 = std::make_unique<keyed_queue<int, int>>();
+      // kq5->push(4, 0);
+      // assert(kq5->front().first == 4 && kq5->front().second == 0);
+      // auto kq6(*kq5);
+      // kq5.reset();
+      // assert(kq6.front().first == 4 && kq6.front().second == 0);
+      //
+      // std::swap(kq1, kq2);
+      // std::vector<keyed_queue<int, int>> vec;
+      // for (int i = 0; i < 100000; i++) {
+      //   kq1.push(i, i);
+      // }
+      // for (int i = 0; i < 1000000; i++) {
+      //   vec.push_back(kq1);  // Wszystkie obiekty w vec współdzielą dane.
+      // }
     }
+    catch (char const *e) {
+      // std::cout << "blah\n";
+      if (kq1) kq1->compare();
+      if (kq2) kq2->compare();
+      if (kq3) kq3->compare();
+    }
+    std::cout << "Passes " << i << "-th exception's test\n";
+  }
 
-    std::cout << "OK!\n";
-    return 0;
+  std::cout << "OK!\n";
+  return 0;
 }
--- examples/exceptions_test/throwers.h
+++ examples/exceptions_test/throwers.h
@@ -6,168 +6,173 @@ static int counter;
 
 class Thrower {
 protected:
-    int value;
-    void tryThrow() const {
-        counter--;
-        if (counter == -1) { // was 0
-            // std::cout << "Throwing!\n";
-            throw "exception()";
-        }
+  int value;
+  void tryThrow() const {
+    counter--;
+    if (counter == -1) { // was 0
+      // std::cout << "Throwing!\n";
+      throw "exception()";
     }
-    Thrower(int ctr, int val) : value(val) { counter = ctr; tryThrow(); }
-    Thrower(int val) : value(val) {tryThrow();}
+  }
+  Thrower(int ctr, int val) : value(val) { counter = ctr; tryThrow(); }
+  Thrower(int val) : value(val) {tryThrow();}
 
 };
 
 class ThrowerKey : private Thrower {
 public:
-    ThrowerKey(int ctr, int val) : Thrower(ctr, val) {}
-    ThrowerKey() : ThrowerKey(42) {}
-    ThrowerKey(int val) : Thrower(val) {}
-    ThrowerKey(ThrowerKey const &o) : Thrower(counter, o.value) {}
-    ThrowerKey(ThrowerKey &&o) : Thrower(counter, o.value) {}
-    void operator &= (ThrowerKey const &o) {
-        value = o.value;
-        tryThrow();
-    }
-    ThrowerKey& operator=(ThrowerKey const&& rhs) = delete;
+  ThrowerKey(int ctr, int val) : Thrower(ctr, val) {}
+  ThrowerKey() : ThrowerKey(42) {}
+  ThrowerKey(int val) : Thrower(val) {}
+  ThrowerKey(ThrowerKey const &o) : Thrower(counter, o.value) {}
+  ThrowerKey(ThrowerKey &&o) : Thrower(counter, o.value) {}
+  void operator &= (ThrowerKey const &o) {
+    value = o.value;
+    tryThrow();
+  }
+  ThrowerKey& operator=(ThrowerKey const&& rhs) = delete;
 
-    bool operator<(ThrowerKey const& rhs) const {
-        tryThrow();
-        return this->value < rhs.value;
-    }
+  bool operator<(ThrowerKey const& rhs) const {
+    tryThrow();
+    return this->value < rhs.value;
+  }
 
-    int &get() {
-        tryThrow();
-        return value;
-    }
-    int const &get() const {
-        tryThrow();
-        return value;
-    }
+  int &get() {
+    return value;
+  }
+  int const &get() const {
+    return value;
+  }
 };
 
 class ThrowerVal : private Thrower {
 public:
-    ThrowerVal(int ctr, int val) : Thrower(ctr, val) {}
-    ThrowerVal(ThrowerVal const &o) : Thrower(counter, o.value) {}
-    ThrowerVal(int val) : Thrower(val) {}
-    ThrowerVal() = delete;
-    ThrowerVal(ThrowerVal &&o) = delete;
-    ThrowerVal& operator=(ThrowerVal const&& rhs) = delete;
-    int &get() {
-        tryThrow();
-        return value;
-    }
-    int const &get() const {
-        tryThrow();
-        return value;
-    }
+  ThrowerVal(int ctr, int val) : Thrower(ctr, val) {}
+  ThrowerVal(ThrowerVal const &o) : Thrower(counter, o.value) {}
+  ThrowerVal(int val) : Thrower(val) {}
+  ThrowerVal() = delete;
+  ThrowerVal(ThrowerVal &&o) = delete;
+  ThrowerVal& operator=(ThrowerVal const&& rhs) = delete;
+  int &get() {
+    return value;
+  }
+  int const &get() const {
+    return value;
+  }
 
-    void customSetter(int a) {
-        value = a;
-    }
+  void customSetter(int a) {
+    value = a;
+  }
 };
 
 
 class testKQ {
 private:
-    keyed_queue<ThrowerKey, ThrowerVal> b;
-    keyed_queue<int, int&> c;
-    using K = ThrowerKey;
-    using V = ThrowerVal;
-    using KQiter = keyed_queue<K, V>::k_iterator;
+  keyed_queue<ThrowerKey, ThrowerVal> b;
+  keyed_queue<int, int&> c;
+  using K = ThrowerKey;
+  using V = ThrowerVal;
+  using KQiter = keyed_queue<K, V>::k_iterator;
 
 public:
-    testKQ() : b(), c() {}
-
-    testKQ(testKQ const &p) : b(p.b), c(p.c) {}
-
-    testKQ(testKQ &&p) : b(p.b), c(p.c) {}
-
-    testKQ &operator=(testKQ &other) {
-        b = other.b;
-        c = other.c;
-        return *this;
+  testKQ() : b(), c() {}
+  
+  testKQ(testKQ const &p) {
+    keyed_queue<ThrowerKey, ThrowerVal> temp(p.b);
+    std::list<std::pair<ThrowerKey, ThrowerVal>> l;
+    while(!temp.empty()) {
+      const std::pair<ThrowerKey, ThrowerVal> p = temp.front();
+      l.push_back(p);
+      temp.pop();
     }
+    for (auto p : l) this->push(p.first.get(), p.second.get());
+  }
 
-    void push(int const &k, int const &v) {
-        b.push(ThrowerKey(k), ThrowerVal(v));
-        c.push(b.back().first.get(), b.back().second.get());
-    };
+  testKQ(testKQ &&p) : b(p.b), c(p.c) {}
 
-    void pop() {
-        b.pop();
-        c.pop();
-    };
+  testKQ &operator=(testKQ &other) {
+    b = other.b;
+    c = other.c;
+    return *this;
+  }
 
-    void pop(K const &k){
-        b.pop(k);
-        c.pop(k.get());
-    };
+  void push(int const &k, int const &v) {
+    b.push(ThrowerKey(k), ThrowerVal(v));
+    c.push(b.back().first.get(), b.back().second.get());
+  };
 
-    void move_to_back(K const &k) {
-        b.move_to_back(k);
-        c.move_to_back(k.get());
-    };
+  void pop() {
+    b.pop();
+    c.pop();
+  };
 
-    std::pair<K const &, V &> front() {
-        return b.front();
-    };
-    std::pair<K const &, V &> back() {
-        return b.back();
-    };
-    std::pair<K const &, V const &> front() const {
-        return b.front();
-    };
-    std::pair<K const &, V const &> back() const {
-        return b.back();
-    };
+  void pop(K const &k){
+    b.pop(k);
+    c.pop(k.get());
+  };
 
-    std::pair<K const &, V &> first(K const &key) {
-        return b.first(key);
-    };
-    std::pair<K const &, V &> last(K const &key) {
-        return b.last(key);
-    };
-    std::pair<K const &, V const &> first(K const &key) const {
-        return b.first(key);
-    };
-    std::pair<K const &, V const &> last(K const &key) const {
-        return b.last(key);
-    };
+  void move_to_back(K const &k) {
+    b.move_to_back(k);
+    c.move_to_back(k.get());
+  };
 
-    size_t size() const noexcept {
-        return b.size();
-    };
+  std::pair<K const &, V &> front() {
+    return b.front();
+  };
+  std::pair<K const &, V &> back() {
+    return b.back();
+  };
+  std::pair<K const &, V const &> front() const {
+    return b.front();
+  };
+  std::pair<K const &, V const &> back() const {
+    return b.back();
+  };
 
-    bool empty() const noexcept {
-        return b.empty();
-    };
+  std::pair<K const &, V &> first(K const &key) {
+    return b.first(key);
+  };
+  std::pair<K const &, V &> last(K const &key) {
+    return b.last(key);
+  };
+  std::pair<K const &, V const &> first(K const &key) const {
+    return b.first(key);
+  };
+  std::pair<K const &, V const &> last(K const &key) const {
+    return b.last(key);
+  };
 
-    void clear() {
-        b.clear();
-        c.clear();
+  size_t size() const noexcept {
+    return b.size();
+  };
+
+  bool empty() const noexcept {
+    return b.empty();
+  };
+
+  void clear() {
+    b.clear();
+    c.clear();
+  }
+
+  size_t count(ThrowerKey const &k) const noexcept {
+    return b.count(k);
+  };
+
+  KQiter k_begin() {
+    return b.k_begin();
+  }
+
+  KQiter k_end() {
+    return b.k_end();
+  }
+
+  void compare() {
+    while (!b.empty() && !c.empty()) {
+      assert(b.front().second.get() == c.front().second);
+      b.pop();
+      c.pop();
     }
-
-    size_t count(ThrowerKey const &k) const noexcept {
-        return b.count(k);
-    };
-
-    KQiter k_begin() {
-        return b.k_begin();
-    }
-
-    KQiter k_end() {
-        return b.k_end();
-    }
-
-    void compare() {
-        while (!b.empty()) {
-            assert(b.front().second.get() == c.front().second);
-            b.pop();
-            c.pop();
-        }
-        assert (b.empty() && c.empty());
-    }
+    assert (b.empty() && c.empty());
+  }
 };
